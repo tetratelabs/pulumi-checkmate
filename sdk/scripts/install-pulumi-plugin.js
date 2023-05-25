472aaba74da2294e71731f08eb699dfcfea89fbb
Liam White <liam@tetrate.io>
2023-05-25 14:32:54 -0700

    Prepare for 1.5.4

--- sdk/scripts/install-pulumi-plugin.js
+++ sdk/scripts/install-pulumi-plugin.js
@@ -17,7 +17,7 @@ var childProcess = require("child_process");
 
 var args = process.argv.slice(2);
 
-if (args.indexOf("v1.5.3") !== -1) {
+if (args.indexOf("v1.5.4") !== -1) {
 	process.exit(0);
 }
 
